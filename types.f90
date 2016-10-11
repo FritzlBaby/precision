b8ae8a9ef82352946a9b7e770156cb6e423b8244
FritzlBaby <stephen-evans24@hotmail.com>
2016-10-11 19:33:47 +0000

    added types module

---
+++ types.f90
@@ -0,0 +1,35 @@
+!######################################!
+!                                      !
+!     Precision Test                   !
+!                                      !
+!     FrB                              !
+!                                      !
+!######################################!
+
+!
+! Description
+! ===========
+! Module for type definitions
+!
+!
+
+module types
+
+    use, intrinsic :: iso_fortran_env
+
+    implicit none
+
+    integer, parameter :: sp = REAL32 ! single precision
+    integer, parameter :: dp = REAL64 ! double precision
+    integer, parameter :: i4 = INT32  ! 4 byte int
+    integer, parameter :: i8 = INT64  ! 8 byte int
+
+end module types
+
+!######################################!
+!                                      !
+!     Precision Test                   !
+!                                      !
+!     FrB                              !
+!                                      !
+!######################################!
